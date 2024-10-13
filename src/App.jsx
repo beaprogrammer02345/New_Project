5c949a51a8726b0c35abda745e5baee2fbd6816b
Annu <annusingh09163@gmail.com>
2024-10-13 22:01:52 +0530

    Intial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Header from './sections/Header.jsx'
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
-    return (
-        <main className="overflow-hidden">
-            <Header />
-
-
-        </main>
-    )
-}
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Download />
+      <Footer />
+    </main>
+  );
+};
 export default App;
